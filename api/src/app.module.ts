b255843cdde3a8dda08b70c2d0f6728f242fa1dd
Le Thanh Tung <thanhtung22012006@gmail.com>
2026-07-22 22:31:15 +0700

    feat: add root health check controller for backend

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { VisualizerModule } from './visualizer/visualizer.module';
 import { RagModule } from './rag/rag.module';
 
 @Module({
   imports: [VisualizerModule, RagModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
